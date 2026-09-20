25b81725324913d45396e5203282925e9163347a
Blitzhagen <matritt1985@gmail.com>
2026-09-20 20:15:32 +0200

    sm7225-common: audio: make BT UI sounds audible on A2DP
    - Ship our own audio_policy_volumes.xml: raise the SYSTEM stream
      headset curve from -18dB to 0dB at max index. AOSP caps sonification
      on DEVICE_CATEGORY_HEADSET (which includes BT_A2DP) at -18dB, leaving
      the already quiet AOSP UI samples at ~-60dBFS - inaudible.
    - Keep mixer outputs warm for 120s (ro.audio.flinger_standbytime_ms):
      the software A2DP encoder drops PCM during cold start (AVDTP start +
      tx_flush), so short SoundPool effects die if the session went idle.

--- common.mk
+++ common.mk
@@ -110,7 +110,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(COMMON_PATH)/audio/configs/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hearing_aid_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
